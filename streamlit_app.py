dd3aadf1a38969375c38d59eebef713e36342017
Oracle Public Cloud User <opc@instance-pd-2023.subnet11132245.vcn11132245.oraclevcn.com>
2026-02-24 18:13:30 +0000

    Fix: Pridany chybejici importy pro WebDriverWait

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,11 @@ from scipy.interpolate import CubicSpline
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
 
 # Nastavení pro server (headless mode)
 chrome_options = Options()
